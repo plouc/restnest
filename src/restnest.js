eacbb88f6852dd281d32711e90f3ad4bf77acf23
Raphaël Benitte <benitteraphael@gmail.com>
2015-02-25 08:10:49 +0100

    Improve restnest

--- src/restnest.js
+++ src/restnest.js
@@ -3,15 +3,24 @@ var _ = require('lodash');
 function _throw(message) { throw new Error(message); }
 
 var restnest = {
-    def({ method = 'GET', path, headers = {}, params = {}, resources = {} }) {
+    /**
+     * Define a new resource
+     */
+    def({ method = 'GET', path, headers = {}, params = {}, schema = [], resources = {} }) {
         var def = {
             method:    method,
             path:      path,
             headers:   headers,
             params:    params,
-            resources: {},
-            schema:    []
+            resources: {}
         };
+
+        def.schema = schema.map(paramDef => {
+            if (!paramDef.key) {
+                _throw(`No key defined for parameter schema ${ JSON.stringify(paramDef) }`);
+            }
+            return _.assign({ location: 'query', required: false }, paramDef);
+        });
 
         if (path) {
             var pathParams = path.match(/:[a-zA-Z0-9]+/g);
@@ -29,6 +38,10 @@ var restnest = {
         return def;
     },
 
+    /**
+     * Collect info for each available resources.
+     * @return {Array.<String>}
+     */
     info(resource, stack = []) {
         stack.push(`${ resource.method } ${ resource.path || 'N/A' }
   headers: ${ JSON.stringify(resource.headers) }
@@ -42,6 +55,13 @@ var restnest = {
         return stack;
     },
 
+    /**
+     * Add resource with given id to the given root resource.
+     * @param {Object} resource
+     * @param {String} id
+     * @param {Object} def
+     * @return {Object} augmented resource
+     */
     add(root, id, def) {
         if (root.resources[id]) { _throw(`resource with id "${ id }" already defined`); }
         root.resources[id] = restnest.def(def);
@@ -49,6 +69,11 @@ var restnest = {
         return root;
     },
 
+    /**
+     * Merge multiple resources and return a new one.
+     * @param {Array.<Object>} resources
+     * @return {Object}
+     */
     merge(resources) {
         // merge options for each resources from top to bottom
         var merged = _.merge({}, ..._.map(resources, resource => _.pick(resource, ['method', 'headers', 'body', 'params'])));
@@ -59,12 +84,23 @@ var restnest = {
         // construct full path from each resource path
         merged.path = _.reduce(resources, (path, resource) => path + (resource.path ? resource.path : ''), '');
 
+        // merge all schema
+        merged.schema = _.reduce(resources, (schema, resource) => schema.concat(resource.schema), []);
+
         return merged;
     },
 
+    /**
+     * Collect each resources from the current tree and return a merged one.
+     * @param {Object} root the definition tree
+     * @param {String} path the path to the resource, dot separated
+     * @param {Array.<Object>} stack collected resources
+     */
     with(root, path, stack = []) {
         var pathParts = path.split('.');
-        if (pathParts.length === 0) { _throw('Invalid path given'); }
+        if (pathParts.length === 0) {
+            _throw('Invalid path given');
+        }
 
         if (stack.length === 0) {
             stack.push(root);
@@ -72,7 +108,9 @@ var restnest = {
 
         var headPath = _.first(pathParts);
 
-        if (!root.resources[headPath]) { _throw(`no resource defined for id "${ headPath }"`); }
+        if (!root.resources[headPath]) {
+            _throw(`no resource defined for id "${ headPath }"`);
+        }
 
         var resource = root.resources[headPath];
 
@@ -80,15 +118,29 @@ var restnest = {
 
         var restParts = _.rest(pathParts);
 
-        // if there is no restParts, we're done, we can return the stack
+        // if there is no tail parts, we're done, we can return the stack
         if (restParts.length === 0) {
             return restnest.merge(stack);
         }
 
+        // recursivity
         return restnest.with(resource, _.rest(pathParts).join('.'), stack);
     },
 
     call(resource, adapter, params = {}) {
+        var schema = resource.schema;
+
+        resource = _.omit(resource, ['schema', 'resources']);
+        _.merge(resource.params, params);
+
+        schema.forEach(paramSchema => {
+            if (paramSchema.required === true && !_.has(resource.params, paramSchema.key)) {
+                _throw(`Parameter '${ paramSchema.key }' must be defined`);
+            }
+
+
+        });
+
         console.log(resource);
     }
 };
